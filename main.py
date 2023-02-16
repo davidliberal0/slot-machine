084c1dbdc7a7a254e174f1afdc0f67f06e2723d6
davidliberal0 <davidliberal0@gmail.com>
2023-02-16 15:51:42 -0500

    add function for getting wager from user

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # Global constants
 MAX_LINES = 3
+MIN_BET = 1
+MAX_BET = 100
 
 # function to collect the deposit as user input
 def deposit():
@@ -34,10 +36,32 @@ def get_number_of_lines():
             print("Please enter a number!")
     return lines
 
+def get_bet():
+    while True:
+        wager = input("What would like to bet? $")
+        # validate user input - make sure it is a number
+        if wager.isdigit():
+            wager = int(wager) # convert the user input to an integer
+            # check if wager falls within the range of min and max bet
+            if MIN_BET <= wager <= MAX_BET:
+                break
+            else:
+                print(f"Wager must be between ${MIN_BET} - ${MAX_BET}")
+        else:
+            print("Please enter a number!")
+    return wager
+
+
+
 
 def main():
     balance = deposit()
     lines = get_number_of_lines()
+    bet = get_bet()
+    # calculate the total bet
+    total_bet = bet * lines
+    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
+
     print(balance, lines)
 
 
