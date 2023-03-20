48c3716a15726c6fc3bec0d85092dd71d7442ef6
davidliberal0 <davidliberal0@gmail.com>
2023-03-19 22:30:52 -0400

    add game winning logic

--- main.py
+++ main.py
@@ -16,10 +16,38 @@ symbol_count = {
     "D": 8
 }
 
+symbol_value = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+
+
 # important -------------------------
 # Difference between row and column
 # ROW -------
 # Column |||||||||
+
+
+def check_winnings(columns, lines, bet, values):
+    winnings = 0
+    winning_lines = []
+    for line in range(lines):
+        # get the symbol at each column
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol == symbol_to_check:
+                break
+            else:
+                # Calculate winnings
+                winnings += values[symbol] * bet
+                winning_lines.append(line + 1)
+
+    return winnings, winning_lines
+
 
 
 
@@ -127,6 +155,9 @@ def main():
 
     slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
     print_slot_machine(slots)
+    winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
+    print(f"You won ${winnings}.")
+    print(f"You won on lines:", *winning_lines)
 
 
 main()
